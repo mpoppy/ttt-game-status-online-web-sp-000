762a93f1d2e3fd1b23e251afe9b10cb291f3e3e7
Marc Poppy <poppy.marc@gmail.com>
2019-04-28 04:51:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,13 +33,13 @@ def won?(board)
     position_3 = board[win_index_3] #store board position 2
 
 
-    if position_taken?(board, position_1) == false && position_taken?(board, position_2) == false && position_taken?(board, position_3) == false
+    if position_taken?(board, position_1) != true && position_taken?(board, position_2) != true && position_taken?(board, position_3) != true
         false
     elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
       return winner
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return winner
-    else 
+    else
       false
     end
   end
