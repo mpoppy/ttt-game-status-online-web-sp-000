7501f86535ad297c61dabd24f41b318055b585f6
Marc Poppy <poppy.marc@gmail.com>
2019-04-28 05:44:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,7 +58,7 @@ def draw?(board)
   full_board = full?(board) #grab true (board full) or false (not full)
   won_board = won?(board) #return winner (if winner) or false (no winner)
 
-    if full_board != false && won_board == !false #
-      true
+    if won_board == false && full_board == false
+      false
     end
 end
