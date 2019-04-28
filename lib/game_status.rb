a9e444a51f958c74325f673aa6c79f0830d2ba90
Marc Poppy <poppy.marc@gmail.com>
2019-04-28 04:28:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,11 +39,11 @@ def won?(board)
       return winner
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return winner
+    elsif board.empty?
+      false
     else
       false
     end
-
-    #binding.pry
   end
 end
 
